cc2038f416ac47698fa70687d37b5b5d8fc333dc
maarja@ryytel.eu <maarja@ryytel.eu>
2019-09-06 18:13:42 +0300

    second commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,7 +16,18 @@ namespace ConsoleApp1
             string name;
             name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
+       
+            Console.WriteLine("When were you born? Please enter the year: ");
+            int year;
+            year = int.Parse(Console.ReadLine());
+            int age = 2019 - year;
+            Console.WriteLine("You are " + age + " years old");
             Console.ReadLine();
+
+
+
+
+
         }
     }
 }
